08e9cd4aa66324e7a24caa6dd5400891f1fb8d45
Holly <holly@hello.com>
2016-11-26 19:45:25 -0800

    added conditionals to game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,34 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+    alert("You found a match! (cardOne = cardTwo) " + cardOne + " " + cardTwo);
+} else {
+    alert ("Sorry, try again. cardOne <> cardTwo " + cardOne + " " + cardTwo);
+}
+if (cardOne === cardThree) {
+    alert("You found a match! (cardOne = cardThree) " + cardOne + " " + cardThree);
+} else {
+    alert ("Sorry, try again. cardOne <> cardThree " + cardOne + " " + cardThree);
+}
+if (cardThree === cardFour) {
+    alert("You found a match! (cardThree = cardFour) " + cardThree + " " + cardFour);
+} else {
+    alert ("Sorry, try again. cardThree <> cardFour " + cardThree + " " + cardFour );
+}
+if (cardTwo === cardThree) {
+    alert("You found a match! (cardTwo= cardThree) " + cardTwo + " " + cardThree );
+} else {
+    alert ("Sorry, try again. cardTwo <> cardThree " + cardTwo + " " + cardThree );
+}
+if (cardTwo === cardFour) {
+    alert("You found a match! (cardTwo= cardFour) " + cardTwo + " " + cardFour );
+} else {
+    alert ("Sorry, try again. cardTwo <> cardFour " + cardTwo + " " + cardFour);
+}
+if (cardThree === cardFour) {
+    alert("You found a match! (cardThree= cardFour) " + cardThree + " " + cardFour);
+} else {
+    alert ("Sorry, try again. cardThree <> cardFour " + cardThree + " " + cardFour);
+}
