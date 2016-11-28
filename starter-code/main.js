36ab37d2e1e0c1baf756eba2b04b5595c282ef33
Holly <holly@hello.com>
2016-11-28 00:16:42 -0800

    completed unit 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,37 +1,45 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
 
-if (cardOne === cardTwo) {
-    alert("You found a match! (cardOne = cardTwo) " + cardOne + " " + cardTwo);
-} else {
-    alert ("Sorry, try again. cardOne <> cardTwo " + cardOne + " " + cardTwo);
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+
 }
-if (cardOne === cardThree) {
-    alert("You found a match! (cardOne = cardThree) " + cardOne + " " + cardThree);
-} else {
-    alert ("Sorry, try again. cardOne <> cardThree " + cardOne + " " + cardThree);
+
+function isTwoCards() {
+
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; 
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; 
+	}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
 }
-if (cardThree === cardFour) {
-    alert("You found a match! (cardThree = cardFour) " + cardThree + " " + cardFour);
-} else {
-    alert ("Sorry, try again. cardThree <> cardFour " + cardThree + " " + cardFour );
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
 }
-if (cardTwo === cardThree) {
-    alert("You found a match! (cardTwo= cardThree) " + cardTwo + " " + cardThree );
-} else {
-    alert ("Sorry, try again. cardTwo <> cardThree " + cardTwo + " " + cardThree );
-}
-if (cardTwo === cardFour) {
-    alert("You found a match! (cardTwo= cardFour) " + cardTwo + " " + cardFour );
-} else {
-    alert ("Sorry, try again. cardTwo <> cardFour " + cardTwo + " " + cardFour);
-}
-if (cardThree === cardFour) {
-    alert("You found a match! (cardThree= cardFour) " + cardThree + " " + cardFour);
-} else {
-    alert ("Sorry, try again. cardThree <> cardFour " + cardThree + " " + cardFour);
-}
+
+createBoard();
